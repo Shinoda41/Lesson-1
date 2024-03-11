3d58ad57a1b74ef6726ed489158f59a5dcbce30f
Shinoda41 <159990677+Shinoda41@users.noreply.github.com>
2024-03-11 13:19:49 +0300

    Add files via upload

---
+++ lesson.py
@@ -0,0 +1,19 @@
+
+name = "Дима"
+num = 123
+random_bool = False
+
+print(name)
+print(num)
+print(random_bool)
+week = ["pom"]
+length =  len(week)
+print(length)
+
+def sum_numbers():
+    print("меня вызвали 1")
+    print("меня вызвали 2")
+    print("меня вызвали 3")
+    print("меня вызвали 4")
+    print("меня вызвали 5")
+
